9d91d54fdde461cfd4e750539740c540be3202f7
aryoda <R@altfeld-im.de>
2016-12-05 21:15:40 +0100

    fixed R CMD check problems due to non-ASCII characters

--- R/tryCatchLog.R
+++ R/tryCatchLog.R
@@ -1,5 +1,5 @@
 # ***************************************************************************
-# Copyright (C) 2016 Jürgen Altfeld (R@altfeld-im.de)
+# Copyright (C) 2016 Juergen Altfeld (R@altfeld-im.de)
 # ---------------------------------------------------------------------------
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
