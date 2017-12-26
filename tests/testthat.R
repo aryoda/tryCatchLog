ee135b8c6d7e851f21bf165d46639610060e7cbd
Devolution <info@altfeld-im.de>
2017-12-26 14:16:42 +0100

    added workaround to ensure english (error) messages in testthat unit tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -8,6 +8,10 @@ library(tryCatchLog)
 # to ensure english error messages
 # DISABLED - DOES NOT WORK (AT LEAST NOT ON OSX)!
 # Sys.setlocale("LC_ALL", "en_US.UTF-8")
+# Sys.getlocale()
+
+# https://stackoverflow.com/questions/47977951/how-to-ensure-english-error-messages-in-testthat-unit-tests
+Sys.setenv("LANGUAGE"="EN")  # work-around to always create english R (error) messages
 
 
 
