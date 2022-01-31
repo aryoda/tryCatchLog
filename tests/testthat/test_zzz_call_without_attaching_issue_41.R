09dc6d5a16f93c04682f3cd8925520baef17a256
Jürgen Altfeld <R@altfeld-im.de>
2022-01-31 01:43:24 +0100

    Fix broken unit test "zzz" if futile.logger 1.4.5 or higher is installed (caused by futile.logger issue #91)

--- tests/testthat/test_zzz_call_without_attaching_issue_41.R
+++ tests/testthat/test_zzz_call_without_attaching_issue_41.R
@@ -35,7 +35,10 @@ test_that("tryCatchLog functions do work without attaching the package with 'lib
 
 
 
-  # if the package is not loaded "detach" throws an error: invalid 'name' argument
+  # If the package is not loaded "detach" throws an error: invalid 'name' argument
+  # Jan 31, 2022 Why detach it if it is not loaded before (at least not obviously)?
+  #              The detach() should always throw an error which is not wanted but obviously does not. Why?
+  #
   # Issue "Can't detach package in tests":
   # THIS IS NOT SUPPORTED BY DEVTOOLS, YOU HAVE TO RUN TESTTHAT WITHOUT DEVTOOLS FOR THIS:
   # https://github.com/r-lib/devtools/issues/1797
@@ -44,13 +47,23 @@ test_that("tryCatchLog functions do work without attaching the package with 'lib
 
   expect_false(isNamespaceLoaded("tryCatchLog"), info = "precondition failed: tryCatchLog package may not be attached")
 
-  expect_output(
+  # NOTE:
+  #
+  # expect_output() no longer works after using futile.logger 1.4.5 or higher
+  # since the log output is written via message() instead of cat() since then
+  # except you set the ENV var 'FUTILE_LOGGER_LEGACY' to TRUE. See:
+  # https://github.com/zatonovo/futile.logger/issues/91
+  # Lesson learned: Unit tests should not depend on other packages (if possible)
+  #                 since their semantics could change and break the unit test!
+  # Jan 31, 2022 Since logging is not under test here I have disabled the log output expectation.
+  # expect_output(  # before futile.logger 1.4.5
+  # expect_message(   # since futile.logger 1.4.5
     expect_error(tryCatchLog::tryCatchLog(stop("hello_error_message")),
                   regexp = "hello_error_message",
                   fixed = TRUE,
                   info = "must throw the stop message, not an unexpected internal error")
-    , info = "the logger must produce an output"
-  )
+  #   , info = "the logger must produce an output"
+  # )
 
   # load the namespace again to avoid side effects on other unit tests.
   # This must be done within the test_that function to prevent side effects on other tests (whyever)
